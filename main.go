cf480b76e43f7b0baca8ac55d001e8690c6c936c
marcos <marcos@tiqets.com>
2019-08-06 17:29:15 +0200

    Add UI class

--- main.go
+++ main.go
@@ -1,13 +1,10 @@
 package main
 
-import "fmt"
 import "bufio"
 import "os"
-import "strings"
 import "github.com/go-redis/redis"
 import "github.com/satori/go.uuid"
 import "encoding/json"
-import tm "github.com/buger/goterm"
 
 type Message struct {
 	Name     string    `json:"name"`
@@ -35,11 +32,6 @@ func read_redis(c chan Message, pubsub *redis.PubSub) {
 	c <- message
 }
 
-func createBox() *tm.Box {
-	chatBox := tm.NewBox(100|tm.PCT, 20, 0)
-	return chatBox
-}
-
 func main() {
 	clientId := uuid.Must(uuid.NewV4())
 
@@ -50,32 +42,16 @@ func main() {
 	})
 	pubsub := client.Subscribe("mychannel")
 
-	tm.Clear()
-	tm.MoveCursor(1, 1)
-	tm.Printf("Type in your name: ")
-	tm.Flush()
-	reader := bufio.NewReader(os.Stdin)
-	name, _ := reader.ReadString('\n')
-	name = strings.TrimSuffix(name, "\n")
+	ui := NewChatUI()
+	name := ui.AskForInput("Type in your name: ")
 
 	c := make(chan Message)
 	history := make([]Message, 0)
 
-	tm.Clear()
-	chatBox := createBox()
-	tm.Print(tm.MoveTo(chatBox.String(), 1, 1))
-	tm.MoveCursor(1, 21)
-	tm.Print("> ")
-	tm.MoveCursor(3, 21)
-	tm.Flush()
-
+	ui.Reset()
+	ui.Flush()
 	for true {
-		tm.Clear()
-		chatBox := createBox()
-		tm.MoveCursor(1, 21)
-		tm.Print("> ")
-		tm.MoveCursor(3, 21)
-
+		ui.Reset()
 		go read(client, name, clientId)
 		go read_redis(c, pubsub)
 		message := <-c
@@ -86,13 +62,11 @@ func main() {
 
 		for index := range history {
 			message := history[index]
-			fmt.Fprintf(chatBox, "%s: %s", message.Name, message.Message)
+			ui.Printf("%s: %s", message.Name, message.Message)
 		}
 		if len(history) >= 18 {
 			history = history[1:]
 		}
-		tm.Print(tm.MoveTo(chatBox.String(), 1, 1))
-		tm.MoveCursor(3, 21)
-		tm.Flush()
+		ui.Flush()
 	}
 }
